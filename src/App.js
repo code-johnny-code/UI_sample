9a4e73df563bf03918f37daeaddf2cd343d2b201
code-johnny-code <john.ross86@live.com>
2021-06-22 16:31:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [cultivoUserId, setCultivoUserId] = useState("bd995496-c475-4307-9efc-7ded22676e03")
+  const [cultivoToken, setCultivoToken] = useState("84f5acde-10ad-4341-90b1-2f9e1821d106")
+  const [paymentMethods, setPaymentMethods] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/methods', {
+      headers: {
+        "authorization": cultivoToken
+      },
+      mode: 'no-cors'
+    })
+      .then(res => res.json())
+      .then(fetchedMethods => setPaymentMethods(fetchedMethods))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {paymentMethods.map(method => {
+        return <p>{method}</p>
+      })}
     </div>
   );
 }
